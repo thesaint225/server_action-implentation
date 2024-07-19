37b13df582dc11be16e95604c67d4c591b66caaf
henrymiessan <miessanhenri@gmail.com>
2024-07-19 07:45:46 +0000

    learn server actions with both client and server components

--- src/app/components/AddButton.tsx
+++ src/app/components/AddButton.tsx
@@ -5,9 +5,15 @@ import { addProductToDatabase } from "../../../actions/serveraction";
 
 function AddButton() {
   const [isPending, startTransition] = useTransition();
+  const formData = new FormData();
+  formData.append("product", "macbook");
+  formData.append("price", "1299");
   return (
-    <button onclick={() => startTransition(() => addProductToDatabase())}>
-      Add product
+    <button
+      onClick={() => startTransition(() => addProductToDatabase(formData))}
+      className="fixed bottom-10 right-10 bg-green-400 text-yellow-100 p-2 rounded-md w-48"
+    >
+      {isPending ? "Adding..." : "Add Mackbook pro"}
     </button>
   );
 }
